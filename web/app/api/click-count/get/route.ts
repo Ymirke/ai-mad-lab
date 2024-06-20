d006dfc3755fc53851361e8aa61023a93a6aa48c
Ymir <36711026+Ymirke@users.noreply.github.com>
2024-06-20 15:36:16 +0200

    fixed ts errors

--- web/app/api/click-count/get/route.ts
+++ web/app/api/click-count/get/route.ts
@@ -3,9 +3,9 @@ import { kv } from '@vercel/kv';
 export const dynamic = 'force-dynamic'; // defaults to auto
 
 export async function GET(req: Request) {
-  const prev: number = await kv.get('click_count');
+  const prev: number | null = await kv.get('click_count');
 
-  const newNum = prev + 1;
+  const newNum = (prev || 0) + 1;
 
   await kv.set('click_count', newNum);
 
