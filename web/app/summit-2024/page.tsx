2c6ab86feaa26f85c3f8079e0c3bd6f34b21b110
Ymir <36711026+Ymirke@users.noreply.github.com>
2024-10-03 15:38:17 +0200

    manually fixing it

--- web/app/summit-2024/page.tsx
+++ web/app/summit-2024/page.tsx
@@ -1,27 +1,5 @@
 import Ticket from './actual-page';
 
-export const metadata = {
-  metadataBase: new URL('https://aimadlab.com/summit-2024'),
-  openGraph: {
-    images: [
-      {
-        url: ogImage.src,
-        width: ogImage.width,
-        height: ogImage.height,
-      },
-    ],
-  },
-  twitter: {
-    images: [
-      {
-        url: ogImage.src,
-        width: ogImage.width,
-        height: ogImage.height,
-      },
-    ],
-  },
-};
-
 export default function Page() {
   return <Ticket />;
 }
